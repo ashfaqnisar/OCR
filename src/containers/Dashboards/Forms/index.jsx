816f0091ccc9fd41395e9b1990fd47468be83e08
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-26 08:45:43 +0530

    Added the grid management to the forms page

--- src/containers/Dashboards/Forms/index.jsx
+++ src/containers/Dashboards/Forms/index.jsx
@@ -1,9 +1,9 @@
-import { Col, Container, Row, Button } from 'reactstrap';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import useSWR from 'swr';
 import axios from 'axios';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
+import { Col, Row, Container } from 'reactstrap';
 
 const ApplicationForms = () => {
   const { uid } = useSelector(state => state.user.data);
@@ -18,47 +18,114 @@ const ApplicationForms = () => {
   const fetchPreviousDocument = () => {};
 
   return (
-    <div>
-      <Container className="dashboard">
-        <Row>
-          <Col sm={6} md={8} lg={10} xl={10}>
-            <h3 className="page-title">Application Forms</h3>
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={2} xl={2}>
-            <Button color="primary" onClick={uploadDocument}>
-              New Form
-            </Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {error ? (
-              <>
-                <p>Error: {error.message}</p>
-                {console.log({ ...error })}
-              </>
-            ) : !documents ? (
-              <>Loading</>
-            ) : (
-              <Grid
-                container
-                justify="space-between"
-                alignItems={'center'}
-                direction="row"
-              >
+    <Grid container className="dashboard container" direction={'column'}>
+      <Grid
+        item
+        container
+        justify={'space-between'}
+        alignItems={'center'}
+        direction={'row'}
+      >
+        <Grid item xs={10} sm={10} md={10} lg={11} xl={11}>
+          <h3 className="page-title">Application Forms</h3>
+        </Grid>
+        <Grid item xs={12} sm={2} md={2} lg={1} xl={1}>
+          <Button color="primary" onClick={uploadDocument}>
+            New Form
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item container>
+        {error ? (
+          <>
+            <Grid item>
+              <p>Error: {error.message}</p>
+            </Grid>
+          </>
+        ) : !documents ? (
+          <Grid item>
+            <p>Loading</p>
+          </Grid>
+        ) : (
+          <Grid item xl={12}>
+            <Container>
+              <Row>
                 {documents.data.map(doc => (
-                  <Grid item key={doc.ocrId}>
+                  <Col lg={3} key={doc.ocrId}>
                     <div>
                       <p>{doc.input}</p>
                     </div>
-                  </Grid>
+                  </Col>
                 ))}
+              </Row>
+            </Container>
+          </Grid>
+
+          /*<Grid
+            container
+            justify="space-between"
+            alignItems={'center'}
+            direction="row"
+          >
+            {documents.data.map(doc => (
+              <Grid item xs={6} sm={6} md={4} lg={4} xl={3} key={doc.ocrId}>
+                <div>
+                  <p>{doc.input}</p>
+                </div>
               </Grid>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    </div>
+            ))}
+          </Grid>*/
+        )}
+      </Grid>
+    </Grid>
+
+    /*<div>
+          <Container className="dashboard">
+            <Row>
+              <Col sm={6} md={8} lg={10} xl={10}>
+                <h3 className="page-title">Application Forms</h3>
+              </Col>
+              <Col xs={12} sm={6} md={4} lg={2} xl={2}>
+                <Button color="primary" onClick={uploadDocument}>
+                  New Form
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              {error ? (
+                <>
+                  <Col>
+                    <p>Error: {error.message}</p>
+                  </Col>
+                </>
+              ) : !documents ? (
+                <>Loading</>
+              ) : (
+                documents.data.map(doc => (
+                  <Col lg={3} key={doc.ocrId}>
+                    <div>
+                      <p>{doc.input}</p>
+                    </div>
+                  </Col>
+                ))
+                /!*<Grid
+                  container
+                  justify="space-between"
+                  alignItems={'center'}
+                  direction="row"
+                >
+                  {documents.data.map(doc => (
+                    <Grid item key={doc.ocrId}>
+                      <div>
+                        <p>{doc.input}</p>
+                      </div>
+                    </Grid>
+                  ))}
+                </Grid>*!/
+              )}
+            </Row>
+          </Container>
+        </div>*/
   );
 };
 
