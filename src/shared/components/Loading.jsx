078f8b5503eb6d789a7102c375f3c21814e2e969
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-31 16:38:56 +0530

    Changed the loading bar

--- src/shared/components/Loading.jsx
+++ src/shared/components/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = ({ loading }) => (
   <div className={`load${loading ? '' : ' loaded'}`}>
     <div className="load__icon-wrap">
       <svg className="load__icon">
-        <path fill="#4ce1b6" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+        <path fill="#794dff" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
       </svg>
     </div>
   </div>
