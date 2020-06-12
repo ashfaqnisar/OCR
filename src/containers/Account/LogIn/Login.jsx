30787418172f4fa924a8eea5d38703bf748f0dfb
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-12 15:29:13 +0530

    Removed the LOgin card file

--- src/containers/Account/LogIn/Login.jsx
+++ src/containers/Account/LogIn/Login.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
-import LoginCard from './components/LoginCard';
+import React, { useState } from 'react';
+import { LogoLoading } from '../../../shared/components/LogoLoading';
+import logo from '../../../images/eslogo.svg';
+import LogInForm from './components/LogInForm';
+import { useHistory } from 'react-router-dom';
+import { useFirebase } from 'react-redux-firebase';
 
-const Login = () => (
-  <div className="account account--not-photo">
-    <LoginCard />
-  </div>
-);
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const history = useHistory();
+  const firebase = useFirebase();
+
+  const signInWithGoogle = (email, password) => {
+    setLoading(true);
+    firebase
+      .login({
+        email: email,
+        password: password
+      })
+      .then(() => {
+        setLoading(false);
+        history.push('/home');
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err.message);
+      });
+  };
+
+  const onSubmitFireBase = ({ email, password }) => {
+    event.preventDefault();
+    signInWithGoogle(email, password);
+  };
+
+  return loading ? (
+    <LogoLoading />
+  ) : (
+    <div className="account account--not-photo">
+      <div className="account__wrapper">
+        <div className="account__card">
+          <div className="account__head">
+            <div className="account__title text-center">
+              <img
+                style={{ align: 'center', width: '50%' }}
+                src={logo}
+                alt="Logo"
+              />
+              <span className="account__logo" />
+            </div>
+          </div>
+          <LogInForm
+            onSubmit={onSubmitFireBase}
+            errorMessage={error}
+            form="log_in_form"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
