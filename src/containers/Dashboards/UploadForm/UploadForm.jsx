c4616fa9dea78e113fad8da71b38a7d734e05bdb
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-13 21:13:48 +0530

    Added the grid to the dropzone

--- src/containers/Dashboards/UploadForm/UploadForm.jsx
+++ src/containers/Dashboards/UploadForm/UploadForm.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Container, Grid, Paper } from '@material-ui/core';
 import Dropzone from 'react-dropzone';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(theme => ({
   dropzoneComponent: {
     height: '200px',
     border: `1px dashed ${theme.colors.black}`,
-    padding: '0px',
     cursor: 'pointer'
   }
 }));
 
 const UploadForm = () => {
   const classes = useStyles();
+  const [files, setFiles] = useState([]);
+
+  const handleDroppedFiles = droppedFiles => {
+    setFiles([...files, ...droppedFiles]);
+  };
+
   return (
     <Container maxWidth={'xl'}>
       <Grid container justify={'center'} spacing={2} direction={'column'}>
@@ -22,24 +28,43 @@ const UploadForm = () => {
         </Grid>
         <Grid item>
           <Paper variant={'outlined'}>
-            <Box p={2}>
-              <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+            <Box p={1}>
+              <Dropzone
+                className="dropzone__input"
+                onDrop={handleDroppedFiles}
+                name={'files'}
+                multiple
+              >
                 {({ getRootProps, getInputProps }) => (
                   <Grid
-                    item
                     container
                     justify={'center'}
                     alignItems={'center'}
-                    {...getRootProps()}
                     className={classes.dropzoneComponent}
+                    {...getRootProps()}
                   >
+                    {(!files || files.length === 0) && (
+                      <Grid item>
+                        <h3>Drop Files Here</h3>
+                      </Grid>
+                    )}
+
                     <input {...getInputProps()} />
-                    <h3>Drop Files Here</h3>
                   </Grid>
                 )}
               </Dropzone>
             </Box>
           </Paper>
+
+          {files.length > 0 && (
+            <Grid item container spacing={2}>
+              {files.map((file, index) => (
+                <Grid item key={index} style={{ zIndex: '1000' }}>
+                  {file.name}
+                </Grid>
+              ))}
+            </Grid>
+          )}
         </Grid>
       </Grid>
     </Container>
