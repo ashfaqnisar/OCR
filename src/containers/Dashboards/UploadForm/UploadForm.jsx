79b2b553a4073c3bc6bc0e00e27a71b036d03f44
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-13 13:16:37 +0530

    Added the dropzone component

--- src/containers/Dashboards/UploadForm/UploadForm.jsx
+++ src/containers/Dashboards/UploadForm/UploadForm.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import { Grid, Container } from '@material-ui/core';
+import { Box, Container, Grid, Paper } from '@material-ui/core';
+import Dropzone from 'react-dropzone';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  dropzoneComponent: {
+    height: '200px',
+    border: `1px dashed ${theme.colors.black}`,
+    padding: '0px',
+    cursor: 'pointer'
+  }
+}));
 
 const UploadForm = () => {
+  const classes = useStyles();
   return (
     <Container maxWidth={'xl'}>
-      <Grid container justify={'flex-start'} alignItems={'center'}>
-        <h3 className="page-title">Upload Form</h3>
+      <Grid container justify={'center'} spacing={2} direction={'column'}>
+        <Grid item>
+          <h3 className="page-title">Upload Form</h3>
+        </Grid>
+        <Grid item>
+          <Paper variant={'outlined'}>
+            <Box p={2}>
+              <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                {({ getRootProps, getInputProps }) => (
+                  <Grid
+                    item
+                    container
+                    justify={'center'}
+                    alignItems={'center'}
+                    {...getRootProps()}
+                    className={classes.dropzoneComponent}
+                  >
+                    <input {...getInputProps()} />
+                    <h3>Drop Files Here</h3>
+                  </Grid>
+                )}
+              </Dropzone>
+            </Box>
+          </Paper>
+        </Grid>
       </Grid>
     </Container>
   );
