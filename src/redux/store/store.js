f30d7ea778c7e3f71a7b9785a828e08a0b8171d5
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-14 12:47:19 +0530

    Added the redux form dependency back

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -4,10 +4,12 @@ import middleware, { history } from './middleware';
 import { sidebar, theme } from '../reducers';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
+import { reducer as form } from 'redux-form';
 
 const reducers = {
   router: connectRouter(history),
   theme,
+  form,
   sidebar,
   firebase: firebaseReducer,
   firestore: firestoreReducer
