11a72d1c9fc6ca35359bdbc144e1352bc7921a3f
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-14 12:47:48 +0530

    Fixed the dropzone

--- src/containers/Dashboards/UploadForm/UploadForm.jsx
+++ src/containers/Dashboards/UploadForm/UploadForm.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Box, Container, Grid, Paper } from '@material-ui/core';
-import Dropzone from 'react-dropzone';
+import { Box, Container, Grid, Paper, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { Field, reduxForm } from 'redux-form';
+import renderDropZoneMultipleField from './components/UploadDropzone';
 
 const useStyles = makeStyles(theme => ({
   dropzoneComponent: {
@@ -13,20 +13,6 @@ const useStyles = makeStyles(theme => ({
 
 const UploadForm = () => {
   const classes = useStyles();
-  const [files, setFiles] = useState([]);
-
-  const [dropzone, setDropzone] = useState(false);
-
-  const array = ['Hello World', 'Hello World', 'Hello World', 'Hello World'];
-
-  const handleDroppedFiles = droppedFiles => {
-    setDropzone(false);
-    setFiles([...files, ...droppedFiles]);
-  };
-
-  const triggerDropzone = () => {
-    setDropzone(true);
-  };
 
   return (
     <Container maxWidth={'xl'}>
@@ -34,118 +20,19 @@ const UploadForm = () => {
         <Grid item>
           <h3 className="page-title">Upload Form</h3>
         </Grid>
-        {files.length > 0 && (
-          <Grid item>
-            <Paper variant={'outlined'}>
-              <Grid container justify={'space-evenly'} spacing={2}>
-                {array.map((text, index) => (
-                  <Grid item xs={6} sm md lg={3} xl key={index}>
-                    <Paper variant={'outlined'}>
-                      <Grid
-                        container
-                        direction={'column'}
-                        alignItems={'center'}
-                        justify={'center'}
-                      >
-                        <Grid item>
-                          <p>text</p>
-                        </Grid>
-                      </Grid>
-                    </Paper>
-                  </Grid>
-                ))}
-              </Grid>
-            </Paper>
-          </Grid>
-        )}
-        <Grid item>
-          <Dropzone
-            onDrop={handleDroppedFiles}
-            name={'files'}
-            onFileDialogCancel={() => setDropzone(false)}
-            multiple
-          >
-            {({ getRootProps, getInputProps }) => (
-              <div {...getRootProps()}>
-                {files && files.length === 0 && (
-                  <>
-                    <Grid
-                      container
-                      justify={'center'}
-                      alignItems={'center'}
-                      className={classes.dropzoneComponent}
-                    >
-                      <Grid item>
-                        <h4>Drop Files Here!</h4>
-                      </Grid>
-                    </Grid>
-                    <input {...getInputProps()} />
-                  </>
-                )}
-                {files.length > 0 && (
-                  <>
-                    <Button color="primary" onClick={triggerDropzone}>
-                      Add More Files
-                    </Button>
-                    {dropzone && <input {...getInputProps()} />}
-                  </>
-                  /*<Grid container justify={'flex-start'} alignItems={'center'}>
-                    <Grid item style={{ border: 'solid red' }}>
-                      <Button color="primary">Add More Files</Button>
-                      <input {...getInputProps()} />
-                    </Grid>
-                  </Grid>*/
-                )}
-              </div>
-            )}
-          </Dropzone>
+        <Grid item lg>
+          <form className={'form'} onSubmit={() => console.log('submitterd')}>
+            <Field name={'files'} component={renderDropZoneMultipleField} />
+            <Button color="primary" type="submit">
+              Submit
+            </Button>
+          </form>
         </Grid>
-
-        {/*<Dropzone onDrop={handleDroppedFiles} name={'files'} multiple>
-                {({ getRootProps, getInputProps }) => {})}
-                    {(files &&  files.length === 0) && (
-                        <Grid container>
-                          
-                        </Grid>
-                    )}
-                  /*<Grid
-                    container
-                    justify={'center'}
-                    alignItems={'center'}
-                    className={classes.dropzoneComponent}
-                    {...getRootProps()}
-                  >
-                    {(!files || files.length === 0) && (
-                      <Grid item>
-                        <h3>Drop Files Here</h3>
-                      </Grid>
-                    )}
-                    {files.length > 0 && (
-                      <Grid item container spacing={2} justify={'space-evenly'}>
-                        {files.map((file, index) => (
-                          <Grid
-                            item
-                            lg={2}
-                            key={index}
-                            container
-                            justify={'center'}
-                            style={{ zIndex: '10000' }}
-                          >
-                            <Grid item>
-                              <Paper variant={'outlined'}>{file.name}</Paper>
-                            </Grid>
-                            <Grid item>
-                              <Button>HE</Button>
-                            </Grid>
-                          </Grid>
-                        ))}
-                      </Grid>
-                    )}
-                    <input {...getInputProps()} />
-                  </Grid>*/}
       </Grid>
     </Container>
   );
 };
 
-export default UploadForm;
+export default reduxForm({
+  form: 'drop_files_form'
+})(UploadForm);
