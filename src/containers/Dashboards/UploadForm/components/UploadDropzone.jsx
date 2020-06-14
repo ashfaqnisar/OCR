3ca123801ebd2711f2e9a97bc8550fa5454c8ac8
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-14 20:10:17 +0530

    Fixed the issue with the grid system

--- src/containers/Dashboards/UploadForm/components/UploadDropzone.jsx
+++ src/containers/Dashboards/UploadForm/components/UploadDropzone.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)'
+  },
+  title: {
+    color: theme.colors.white
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
+  },
+  image: {
+    height: '325px !important',
+    position: 'relative',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    margin: theme.spacing(2)
+  }
+}));
 
 const DropZoneMultipleField = props => {
   const { name, value, onChange } = props;
@@ -23,6 +58,7 @@ const DropZoneMultipleField = props => {
 
   const files = value;
 
+  const classes = useStyles();
   return (
     <div className="dropzone dropzone--multiple">
       <Dropzone
@@ -45,6 +81,41 @@ const DropZoneMultipleField = props => {
         )}
       </Dropzone>
       {files && Array.isArray(files) && (
+        <Grid container alignItems={'center'} justify={'center'}>
+          <GridList
+            cellHeight={200}
+            className={'dropzone__imgs-wrapper'}
+            cols={6}
+            style={{
+              border: 'solid red',
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              overflow: 'auto',
+              maxHeight: '600px'
+            }}
+          >
+            {files.map((file, index) => (
+              <GridListTile
+                key={index}
+                className={classes.image}
+                style={{ backgroundImage: `url(${file.preview})` }}
+              >
+                <GridListTileBar
+                  title={file.name}
+                  classes={{ root: classes.titlebar, title: classes.title }}
+                  actionIcon={
+                    <IconButton onClick={e => removeFile(index, e)}>
+                      <DeleteOutlineIcon />
+                    </IconButton>
+                  }
+                />
+              </GridListTile>
+            ))}
+          </GridList>
+        </Grid>
+      )}
+
+      {/*{files && Array.isArray(files) && (
         <div className="dropzone__imgs-wrapper">
           {files.map((file, i) => (
             <div
@@ -63,7 +134,7 @@ const DropZoneMultipleField = props => {
             </div>
           ))}
         </div>
-      )}
+      )}*/}
     </div>
   );
 };
