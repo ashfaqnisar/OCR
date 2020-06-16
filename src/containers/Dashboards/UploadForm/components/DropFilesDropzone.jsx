e0c14f5420fd7b91a9466e2beeb6d9c3aa2b9ecb
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-16 20:38:18 +0530

    Added the handling of the multiple files

--- src/containers/Dashboards/UploadForm/components/DropFilesDropzone.jsx
+++ src/containers/Dashboards/UploadForm/components/DropFilesDropzone.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import renderDropZoneMultipleField from './UploadDropzone';
-import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(1)
-  }
-}));
 const DropFilesDropzone = ({ handleSubmit }) => {
-  const classes = useStyles();
   return <Field name={'files'} component={renderDropZoneMultipleField} />;
 };
 
