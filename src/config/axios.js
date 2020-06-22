5ccdb0b80ee1d9e6519a7ae12608bfeef410dcab
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-22 19:25:11 +0530

    Updated the api link

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:8080';
 } else if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'https://ezerka-ocr-api.herokuapp.com/';
+  axios.defaults.baseURL = 'https://esocr-api.herokuapp.com/';
 }
 
 export default axios;
