9a2c78274d810244ed5baf2333f342158ca89bd3
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-22 19:08:48 +0530

    Removed unrequired dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,16 @@ import { render } from 'react-dom';
 import App from './containers/App/App';
 import * as Sentry from '@sentry/browser';
 
+function getEnvironment() {
+  if (process.env.NODE_ENV === 'development') {
+    return 'development';
+  } else if (process.env.NODE_ENV === 'production') {
+    return 'production';
+  }
+}
+
 Sentry.init({
+  environment: getEnvironment(),
   dsn:
     'https://f632cbf77922400295c68e0e115da45a@o361783.ingest.sentry.io/5240339'
 });
